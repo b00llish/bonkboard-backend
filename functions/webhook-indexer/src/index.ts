9e3bde61ed5cad4c097fee310ebf34948f9a20b5
b00llish <b00llish@pm.me>
2023-07-18 23:33:38 -0500

    cleaning up buys

--- functions/webhook-indexer/src/index.ts
+++ functions/webhook-indexer/src/index.ts
@@ -4,18 +4,29 @@ import {firestore} from 'firebase-admin';
 import {getFirestore} from 'firebase-admin/firestore';
 import {app} from './firebase';
 import {FOXY_RAFFLE_IDL} from './IDL';
-import {DrawInstructionDecoded, TransactionResponseJson} from './types';
+import {
+  TransactionResponseJson,
+  createRaffleInstructionDecoded,
+  buyTicketsInstructionDecoded,
+  addPrizeV2InstructionDecoded,
+  revealWinnersInstructionDecoded,
+  closeEntrantsInstructionDecoded,
+} from './types';
 import {FR_CODER, FR_PROGRAM_ID, CONNECTION, getAccountKey} from './utils';
 
-import './env';
+import axios from 'axios';
+// This could be a UUID or any secret token.
+const secretToken = 'your-secret-token-here';
+
+// import './env';
 
 export async function handleWebhookIndexer(req: Request, res: Response) {
   const txResponses: TransactionResponseJson[] = req.body;
 
   for (const txResponse of txResponses) {
     const signature = txResponse.transaction.signatures[0];
-    const slot = txResponse.slot;
-    const blocktime = txResponse.blockTime;
+    // const slot = txResponse.slot;
+    const createdTimestamp = txResponse.blockTime;
 
     if (txResponse.meta && txResponse.meta.err !== null) {
       continue;
@@ -30,108 +41,461 @@ export async function handleWebhookIndexer(req: Request, res: Response) {
       const ixProgram = accountKeys[ix.programIdIndex];
 
       if (ixProgram !== FR_PROGRAM_ID.toString()) {
+        // console.log(`Program ID ${ixProgram} is not Foxy Raffle`);
         continue;
       }
 
       const decodedIx = FR_CODER.instruction.decode(bs58.decode(ix.data));
+
 
       if (!decodedIx) {
         console.log(`Failed to decode instruction for signature ${signature}`);
         continue;
       }
 
-      if (decodedIx.name === 'draw') {
-        const drawIx = decodedIx.data as DrawInstructionDecoded;
+      if (decodedIx.name === 'createRaffle') {
+        const createRaffleIx = decodedIx.data as createRaffleInstructionDecoded;
+        console.log(`createRaffleIx: ${JSON.stringify(createRaffleIx)}`);
 
-        const user = getAccountKey(
+        const host_wallet = getAccountKey(
           FOXY_RAFFLE_IDL,
-          'payer',
-          'draw',
+          'creator',
+          'createRaffle',
           ix.accounts,
           accountKeys
         );
 
-        const boardAccount = getAccountKey(
+        console.log(`Host wallet: ${host_wallet.toString()}`);
+
+        // unsure
+        // const raffler = getAccountKey(
+        //   FOXY_RAFFLE_IDL,
+        //   'raffler',
+        //   'createRaffle',
+        //   ix.accounts,
+        //   accountKeys
+        // );
+
+        // console.log(`raffler: ${raffler.toString()}`);
+
+        const raffleAccount = getAccountKey(
           FOXY_RAFFLE_IDL,
-          'boardAccount',
-          'draw',
+          'raffle',
+          'createRaffle',
           ix.accounts,
           accountKeys
         );
 
-        let lastSeenSlot = 0;
-        let encodedBoardState: string | undefined = undefined;
-        while (lastSeenSlot < slot) {
-          const result = await CONNECTION.getAccountInfoAndContext(
-            boardAccount,
-            'confirmed'
+        console.log(`Raffle account: ${raffleAccount.toString()}`);
+
+        const proceedsMint = getAccountKey(
+          FOXY_RAFFLE_IDL,
+          'proceedsMint',
+          'createRaffle',
+          ix.accounts,
+          accountKeys
           );
 
-          encodedBoardState = bs58.encode(result.value!.data);
-          lastSeenSlot = result.context.slot;
+        console.log(`Proceeds mint: ${proceedsMint.toString()}`);
 
-          if (lastSeenSlot >= slot) {
-            break;
+        const endTimestamp = createRaffleIx.endTimestamp;
+        console.log(`End timestamp: ${endTimestamp.toString()}`);
+
+        const ticketPrice = createRaffleIx.ticketPrice;
+        console.log(`Ticket price: ${ticketPrice.toString()}`);
+
+        const maxEntrants = createRaffleIx.maxEntrants;
+        console.log(`Max entrants: ${maxEntrants.toString()}`);
+
+        // probably a limit on the number of tickets a user can buy
+        // const limit = createRaffleIx.limit;
+        // console.log(`Limit: ${limit.toString()}`);
+
+        let raffle = {
+          
+          host_wallet: host_wallet.toString(),
+          // creator: creator.toString(),
+          account: raffleAccount.toString(),
+          payment_mint: proceedsMint.toString(),
+          end_epoch: endTimestamp.toString(),
+          tix_price: ticketPrice.toString(),
+          tix_total: maxEntrants.toString(),
+          // limit: limit.toString(),
+          tx_id: signature.toString(),
+          // slot: slot.toString(),
+          start_epoch: createdTimestamp ? createdTimestamp.toString() : undefined,
+        };
+
+        if (createdTimestamp != null) {
+          raffle['start_epoch'] = createdTimestamp.toString();
           } else {
-            await new Promise(resolve => setTimeout(resolve, 250));
+          console.log(`createdTimestamp is null or undefined for tx: ${signature}`);
+        }
+
+        axios.post(
+          'https://raffflytics.ngrok.dev/rcv-raffles-gcp',
+          {
+            type: 'createRaffle',
+            data: raffle,
+          },
+          {
+            headers: {
+                'Authorization': `Bearer ${secretToken}`,
+            }
           }
-        }
+          )
+          .then((res) => {
+              console.log(`Status: ${res.status}`);
+              console.log('Body: ', res.data);
+          }).catch((err) => {
+              console.error(err);
+          });
 
-        const fs = getFirestore(app);
-        const userRef = fs.collection('users').doc(user.toString());
-        const statsRef = fs.collection('stats').doc('mainnet-beta');
-        const activitiesRef = fs.collection('activity').doc(signature);
-        const boardRef = fs.collection('board').doc('mainnet-beta');
+        // const proceedsMint = getAccountKey(
+        //   FOXY_RAFFLE_IDL,
+        //   'raffle',
+        //   'proceedsMint',
+        //   ix.accounts,
+        //   accountKeys
+        // );
+        
+        // console.log(`Proceeds mint: ${proceedsMint.toString()}`);
 
-        const numOfPixelsPlaced = drawIx.pixels.length;
-        const bonkBurned = 0;
-        const bonkPaid = drawIx.pixels.length * 10_000;
 
-        for (const pixel of drawIx.pixels) {
-          console.log(
-            `User: ${user}, Pixel position: ${pixel.coord.x}, ${pixel.coord.y}, Pixel color: ${pixel.color.r} ${pixel.color.g} ${pixel.color.b}`
-          );
-        }
-
-        await Promise.all([
-          activitiesRef.set({
-            slot: slot,
-            signature: signature,
-            timestamp: blocktime,
-            user: user.toString(),
-            activities: drawIx.pixels.map(pixel => ({
-              x: pixel.coord.x,
-              y: pixel.coord.y,
-              rgb: `rgb(${pixel.color.r}, ${pixel.color.g}, ${pixel.color.b})`,
-            })),
-          }),
-          userRef.set(
-            {
-              lastUpdatedSlot: slot,
-              lastUpdatedSignature: signature,
-              lastUpdated: blocktime,
-              pixelsPlaced: firestore.FieldValue.increment(numOfPixelsPlaced),
-              bonkBurned: firestore.FieldValue.increment(bonkBurned),
-              bonkPaid: firestore.FieldValue.increment(bonkPaid),
-            },
-            {merge: true}
-          ),
-          statsRef.set(
-            {
-              pixelsPlaced: firestore.FieldValue.increment(numOfPixelsPlaced),
-              bonkBurned: firestore.FieldValue.increment(bonkBurned),
-              bonkPaid: firestore.FieldValue.increment(bonkPaid),
-            },
-            {merge: true}
-          ),
-          boardRef.set({
-            lastUpdatedSlot: slot,
-            lastUpdatedSignature: signature,
-            lastUpdated: blocktime,
-            boardState: encodedBoardState,
-          }),
-        ]);
       }
+
+      if (decodedIx.name === 'addPrizeV2') {
+        const addPrizeIx = decodedIx.data as addPrizeV2InstructionDecoded;
+        console.log(`buyTicketsIx: ${JSON.stringify(addPrizeIx)}`);
+
+        const raffleAccount = getAccountKey(
+          FOXY_RAFFLE_IDL,
+          'raffle',
+          'addPrizeV2',
+          ix.accounts,
+          accountKeys
+        );
+
+        console.log(`Added prize to raffle: ${raffleAccount.toString()}`);
+
+        const prizeMint = getAccountKey(
+          FOXY_RAFFLE_IDL,
+          'prizeMint',
+          'addPrizeV2',
+          ix.accounts,
+          accountKeys
+        );
+
+        console.log(`NFT mint: ${prizeMint.toString()}`);
+
+        // does not appear to actually be metadata, e.g., Cc2ML8qu1AktwiFhvLPJQpy2AT4k2VpMWw3gggx1eTgr
+        // const nftMetadata = getAccountKey(
+        //   FOXY_RAFFLE_IDL,
+        //   'nftMetadata',
+        //   'addPrizeV2',
+        //   ix.accounts,
+        //   accountKeys
+        // );
+
+        // console.log(`NFT metadata: ${nftMetadata.toString()}`);
+
+        // also not sure what this is, e.g., HWdrD3octHGH53R9F9WiNqVjai93C6dLuqdnXicxGWL6
+        // const edition = getAccountKey(
+        //   FOXY_RAFFLE_IDL,
+        //   'edition',
+        //   'addPrizeV2',
+        //   ix.accounts,
+        //   accountKeys
+        // );
+
+        // console.log(`NFT edition: ${edition.toString()}`);
+
+        let nft = {
+          account: raffleAccount.toString(),
+          nft_mint: prizeMint.toString(),
+          // metadata: nftMetadata.toString(),
+          // tx_id: signature.toString(),
+        };
+
+
+        axios.post('https://raffflytics.ngrok.dev/rcv-raffles-gcp',
+        {
+          type: 'addPrize',
+          data: nft,
+        }, 
+        {
+        headers: {
+            'Authorization': `Bearer ${secretToken}`,
+        }
+        })
+        .then((res) => {
+            console.log(`Status: ${res.status}`);
+            console.log('Body: ', res.data);
+        }).catch((err) => {
+            console.error(err);
+        }); 
+      }
+      
+      if (decodedIx.name === 'buyTickets') {
+        const buyTicketsIx = decodedIx.data as buyTicketsInstructionDecoded;
+        // console.log(`buyTicketsIx: ${JSON.stringify(buyTicketsIx)}`);
+    
+        let pmt_mint: string | undefined = undefined;
+        let pmt_decimals: string | undefined = undefined;
+
+        const postTokenBalances = txResponse?.meta?.postTokenBalances;
+        if (postTokenBalances && postTokenBalances.length > 0) {
+            pmt_mint = postTokenBalances[0]?.mint?.toString();
+            pmt_decimals = postTokenBalances[0]?.uiTokenAmount?.decimals?.toString();
+    
+            console.log(`Payment mint: ${pmt_mint}`);
+            console.log(`Payment decimals: ${pmt_decimals}`);
+        } else {
+            console.log(`postTokenBalances is empty or doesn't exist.`);
+        }
+
+        const raffleAccount = getAccountKey(
+          FOXY_RAFFLE_IDL,
+          'raffle',
+          'buyTickets',
+          ix.accounts,
+          accountKeys
+        );
+
+        // console.log(`Raffle purchased: ${raffleAccount.toString()}`);
+        
+        // buyer token account:
+        // const entrants = getAccountKey(
+        //   FOXY_RAFFLE_IDL,
+        //   'entrants',
+        //   'buyTickets',
+        //   ix.accounts,
+        //   accountKeys
+        // );
+
+        // console.log(`Entrants (buyTickets): ${entrants.toString()}`);
+
+        // raffle token account:
+        // const proceeds = getAccountKey(
+        //   FOXY_RAFFLE_IDL,
+        //   'proceeds',
+        //   'buyTickets',
+        //   ix.accounts,
+        //   accountKeys
+        // );
+
+        // console.log(`Proceeds (buyTickets): ${proceeds.toString()}`);
+        
+        // ??? perhaps ticket account?
+        // const entry = getAccountKey(
+        //   FOXY_RAFFLE_IDL,
+        //   'entry',
+        //   'buyTickets',
+        //   ix.accounts,
+        //   accountKeys
+        // );
+
+        // console.log(`Entry (buyTickets): ${entry.toString()}`);
+
+        // buyer token account:
+        // const buyerTokenAccount = getAccountKey(
+        //   FOXY_RAFFLE_IDL,
+        //   'buyerTokenAccount',
+        //   'buyTickets',
+        //   ix.accounts,
+        //   accountKeys
+        // );
+
+        // console.log(`Buyer token account (buyTickets): ${buyerTokenAccount.toString()}`);
+
+        const buyerTransferAuthority = getAccountKey(
+            FOXY_RAFFLE_IDL,
+            'buyerTransferAuthority',
+            'buyTickets',
+            ix.accounts,
+            accountKeys
+          );
+
+        // console.log(`Buyer transfer authority (buyTickets): ${buyerTransferAuthority.toString()}`);
+
+        // literally the token program
+        // const tokenProgram = getAccountKey(
+        //   FOXY_RAFFLE_IDL,
+        //   'tokenProgram',
+        //   'buyTickets',
+        //   ix.accounts,
+        //   accountKeys
+        // );
+
+        // console.log(`Token program (buyTickets): ${tokenProgram.toString()}`);
+
+        // number of tickets (pretty sure):
+        const amount = buyTicketsIx.amount;
+        // console.log(`Purchase amount: ${amount.toString()}`);
+
+        // price per ticket:
+        const price = buyTicketsIx.price;
+        // console.log(`Purchase price: ${price.toString()}`);
+
+        let buy = {
+          //dt_buy: // calculated from epoch_time
+          buyer_wallet: buyerTransferAuthority.toString(),
+          account: raffleAccount.toString(),
+          // amt_buy: // calculated from amount and price
+          tx_id: signature.toString(),        
+          epoch_time: createdTimestamp ? createdTimestamp.toString() : undefined,
+          payment_mint: pmt_mint ? pmt_mint.toString() : undefined,
+          payment_decimals: pmt_decimals ? pmt_decimals.toString() : undefined,
+          tix_price: price.toString(),
+          tix_buy: amount.toString(),
+        };
+
+        if (createdTimestamp != null) {
+          buy['epoch_time'] = createdTimestamp.toString();
+        } else {
+          console.log(`createdTimestamp is null or undefined for tx: ${signature}`);
+        }
+
+        axios.post('https://raffflytics.ngrok.dev/rcv-buys-gcp', buy, {
+        headers: {
+            'Authorization': `Bearer ${secretToken}`,
+        }
+        })
+        .then((res) => {
+            console.log(`Status: ${res.status}`);
+            console.log('Body: ', res.data);
+        }).catch((err) => {
+            console.error(err);
+        });
+    }
+    
+    if (decodedIx.name === 'revealWinners') {
+      const revealWinnersIx = decodedIx.data as revealWinnersInstructionDecoded;
+      console.log(`revealWinnersIx: ${JSON.stringify(revealWinnersIx)}`);
+      
+      const raffle = getAccountKey(
+          FOXY_RAFFLE_IDL,
+          'raffle',
+          'revealWinners',
+          ix.accounts,
+          accountKeys
+        );
+
+      console.log(`Winner of: ${raffle.toString()}`);
+
+      const entrants = getAccountKey(
+        FOXY_RAFFLE_IDL,
+        'entrants',
+        'revealWinners',
+        ix.accounts,
+        accountKeys
+      );
+
+      console.log(`entrants (winner): ${entrants.toString()}`);
+
+      const signer = getAccountKey(
+        FOXY_RAFFLE_IDL,
+        'signer',
+        'revealWinners',
+        ix.accounts,
+        accountKeys
+      );
+
+      console.log(`signer (winner): ${signer.toString()}`);
+    }
+    
+    if (decodedIx.name === 'closeEntrants') {
+      const closeEntrantsIx = decodedIx.data as closeEntrantsInstructionDecoded;
+      console.log(`closeEntrantsIx: ${JSON.stringify(closeEntrantsIx)}`);
+      
+      const raffle = getAccountKey(
+          FOXY_RAFFLE_IDL,
+          'raffle',
+          'closeEntrants',
+          ix.accounts,
+          accountKeys
+        );
+
+      console.log(`Raffle (closeEntrants): ${raffle.toString()}`);
+
+    // if not a category handled above, log the instruction data
+    console.log(`Decoded instruction: ${JSON.stringify(decodedIx)}`);
+    
+      }
+      // this code below was all within the create raffle end bracket
+        // let lastSeenSlot = 0;
+        // let encodedBoardState: string | undefined = undefined;
+        // while (lastSeenSlot < slot) {
+        //   const result = await CONNECTION.getAccountInfoAndContext(
+        //     boardAccount,
+        //     'confirmed'
+        //   );
+
+        //   encodedBoardState = bs58.encode(result.value!.data);
+        //   lastSeenSlot = result.context.slot;
+
+        //   if (lastSeenSlot >= slot) {
+        //     break;
+        //   } else {
+        //     await new Promise(resolve => setTimeout(resolve, 250));
+        //   }
+        // }
+
+        // const fs = getFirestore(app);
+        // const userRef = fs.collection('users').doc(user.toString());
+        // const statsRef = fs.collection('stats').doc('mainnet-beta');
+        // const activitiesRef = fs.collection('activity').doc(signature);
+        // const boardRef = fs.collection('board').doc('mainnet-beta');
+
+        // const numOfPixelsPlaced = drawIx.pixels.length;
+        // const bonkBurned = 0;
+        // const bonkPaid = drawIx.pixels.length * 10_000;
+
+        // for (const pixel of drawIx.pixels) {
+        //   console.log(
+        //     `User: ${user}, Pixel position: ${pixel.coord.x}, ${pixel.coord.y}, Pixel color: ${pixel.color.r} ${pixel.color.g} ${pixel.color.b}`
+        //   );
+        // }
+
+        // await Promise.all([
+        //   activitiesRef.set({
+        //     slot: slot,
+        //     signature: signature,
+        //     timestamp: blocktime,
+        //     user: user.toString(),
+        //     activities: drawIx.pixels.map(pixel => ({
+        //       x: pixel.coord.x,
+        //       y: pixel.coord.y,
+        //       rgb: `rgb(${pixel.color.r}, ${pixel.color.g}, ${pixel.color.b})`,
+        //     })),
+        //   }),
+        //   userRef.set(
+        //     {
+        //       lastUpdatedSlot: slot,
+        //       lastUpdatedSignature: signature,
+        //       lastUpdated: blocktime,
+        //       pixelsPlaced: firestore.FieldValue.increment(numOfPixelsPlaced),
+        //       bonkBurned: firestore.FieldValue.increment(bonkBurned),
+        //       bonkPaid: firestore.FieldValue.increment(bonkPaid),
+        //     },
+        //     {merge: true}
+        //   ),
+        //   statsRef.set(
+        //     {
+        //       pixelsPlaced: firestore.FieldValue.increment(numOfPixelsPlaced),
+        //       bonkBurned: firestore.FieldValue.increment(bonkBurned),
+        //       bonkPaid: firestore.FieldValue.increment(bonkPaid),
+        //     },
+        //     {merge: true}
+        //   ),
+        //   boardRef.set({
+        //     lastUpdatedSlot: slot,
+        //     lastUpdatedSignature: signature,
+        //     lastUpdated: blocktime,
+        //     boardState: encodedBoardState,
+        //   }),
+        // ]);
+      
     }
   }
 
--- functions/webhook-indexer/src/types.ts
+++ functions/webhook-indexer/src/types.ts
@@ -23,10 +23,65 @@ export type Raffle = TypeDef<
   IdlTypes<FoxyRaffleProgram>
 >;
 
-type createRaffleIxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[1]['args'];
+export type Entrants = TypeDef<
+  (typeof FOXY_RAFFLE_IDL.accounts)[1],
+  IdlTypes<FoxyRaffleProgram>
+>;
+
+export type Entry = TypeDef<
+  (typeof FOXY_RAFFLE_IDL.accounts)[2],
+  IdlTypes<FoxyRaffleProgram>
+>;
+
+export type Raffler = TypeDef<
+  (typeof FOXY_RAFFLE_IDL.accounts)[3],
+  IdlTypes<FoxyRaffleProgram>
+>;
+
+type createRaffleIxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[0]['args'];
 export type createRaffleInstructionDecoded = {
   [K in createRaffleIxArguments[number] as K['name']]: DecodeType<
     K['type'],
     IdlTypes<FoxyRaffleProgram>
   >;
 };
+
+// type extendRaffleIxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[1]['args'];
+// type updateRaffleIxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[2]['args'];
+
+type buyTicketsIxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[3]['args'];
+export type buyTicketsInstructionDecoded = {
+  [K in buyTicketsIxArguments[number] as K['name']]: DecodeType<
+    K['type'],
+    IdlTypes<FoxyRaffleProgram>
+  >;
+};
+
+type revealWinnersIxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[4]['args'];
+export type revealWinnersInstructionDecoded = {
+  [K in revealWinnersIxArguments[number] as K['name']]: DecodeType<
+    K['type'],
+    IdlTypes<FoxyRaffleProgram>
+  >;
+};
+
+type closeEntrantsIxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[5]['args'];
+export type closeEntrantsInstructionDecoded = {
+  [K in closeEntrantsIxArguments[number] as K['name']]: DecodeType<
+    K['type'],
+    IdlTypes<FoxyRaffleProgram>
+  >;
+};
+
+type addPrizeV2IxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[6]['args'];
+export type addPrizeV2InstructionDecoded = {
+  [K in addPrizeV2IxArguments[number] as K['name']]: DecodeType<
+    K['type'],
+    IdlTypes<FoxyRaffleProgram>
+  >;
+};
+
+
+// type cancelRaffleV2IxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[7]['args'];
+// type claimPrizeV2IxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[8]['args'];
+// type collectProceedsV2IxArguments = (typeof FOXY_RAFFLE_IDL.instructions)[9]['args'];
